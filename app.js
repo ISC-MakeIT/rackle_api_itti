0d34d32c636368c5b653adc652df7bf153b65a75
itti1021 <itti.noritokann@gmail.com>
2019-01-23 14:25:52 +0900

    ES6への修正&ルーティング

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +16,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/*ルーティング*/
+
+// 初期
+const indexRouter = require('./routes/index');
+app.use('/api', indexRouter);
+
+// 入力画面
+const stationsRouter = require('./routes/stations');
+app.use('/api/stations', stationsRouter);
+
+// ガイドライン表示用
+const guidelinesRouter = require('./routes/guidelines');
+app.use('/api/guidelines', guidelinesRouter);
+
+/*ここまで*/
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/guidelines.js
@@ -0,0 +1,139 @@
+var express = require('express');
+var router = express.Router();
+
+/* DB設定 */
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host : 'localhost',
+  user : 'root',
+  database : 'rackle'
+});
+
+
+router.get('/:start_gate_id/:end_gate_id', (req, res, next) => {
+    const start_gate_id = req.params.start_gate_id;
+    const end_gate_id = req.params.end_gate_id;
+    // console.log(start_gate_id);
+    // console.log(end_gate_id);
+
+    const get_start_gate = new Promise((resolve, reject) => {
+        const sql = `
+            SELECT
+                id,
+                name,
+                latitude,
+                longitude,
+                floor
+            FROM
+                gates
+            WHERE
+                id = ?`
+        connection.query(sql, [start_gate_id], (err, rows) => {
+            resolve(rows);
+        });
+    });
+
+    const get_end_gate = new Promise((resolve, reject) => {
+        const sql = `
+            SELECT
+                id,
+                name,
+                latitude,
+                longitude,
+                floor
+            FROM
+                gates
+            WHERE
+                id = ?`
+        connection.query(sql, [end_gate_id], (err, rows) => {
+            resolve(rows);
+        });
+    });
+
+    const get_guidelinse = new Promise((resolve, reject) => {
+        const sql = `
+            SELECT
+                location_points.latitude,
+                location_points.longitude,
+                location_points.floor
+            FROM (
+                guidelines
+            INNER JOIN
+                guidelines_location_points
+            ON
+                guidelines.id = guidelines_location_points.guideline_id)
+            INNER JOIN
+	            location_points
+            ON
+                guidelines_location_points.location_point_id = location_points.id
+            WHERE
+                guidelines.start_gate_id = ? AND
+                guidelines.end_gate_id = ?
+            ORDER BY
+                guidelines_location_points.sort;`
+
+        connection.query(sql, [start_gate_id, end_gate_id], (err, rows) => {
+            resolve(rows);
+        });
+    });
+
+    const get_toilets = new Promise((resolve, reject) => {
+        const sql = `
+            SELECT
+                id,
+                latitude,
+                longitude,
+                floor
+            FROM
+                toilets`
+
+        connection.query(sql, (err, rows) => {
+            resolve(rows);
+        });
+    });
+
+    const get_elevator = new Promise((resolve, reject) => {
+        const sql = `
+            SELECT
+                id,
+                size,
+                latitude,
+                longitude,
+                floor
+            FROM
+                elevators`
+
+        connection.query(sql, (err, rows) => {
+            resolve(rows);
+        });
+    });
+
+    Promise.all([get_start_gate, get_end_gate, get_guidelinse, get_toilets, get_elevator]).then((values) => {
+        // console.log(values);
+        // console.log(values[0]);
+        // console.dir(values[2][0]);
+        // const guidelines = values[2];
+        // console.log(guidelines[0]);
+
+        let start_gate, end_gate = {};
+        let guidelines, toilets, elevators = [];
+
+        start_gate = values[0][0];
+        end_gate = values[1][0];
+        guidelines = values[2];
+        toilets = values[3];
+        elevators = values[4];
+
+        let json = {
+            start_gate : start_gate,
+            end_gate : end_gate,
+            guidelines : guidelines,
+            toilets : toilets,
+            elevators : elevators
+        };
+        res.header('Content-Type', 'application/json; charset=UTF-8');
+        res.json(json);
+    });
+});
+
+module.exports = router;
