9453425948fe31e80f522f707a1984d6226797ac
itti1021 <itti.noritokann@gmail.com>
2019-01-23 15:22:06 +0900

    jade削除

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const logger = require('morgan');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
