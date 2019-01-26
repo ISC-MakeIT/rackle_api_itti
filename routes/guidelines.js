d6fa1084d1e0da0c2fe740d45af68e5cae4b7223
itti1021 <itti.noritokann@gmail.com>
2019-01-26 17:11:31 +0900

    envに変更

--- routes/guidelines.js
+++ routes/guidelines.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const mysql = require('mysql');
+require('dotenv').config();
 const connection = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    database : 'rackle'
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    database : process.env.DB_DATABASE
 });
 
 
